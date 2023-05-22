2f0a6f96feffd8fc763f9aa7580bf61ff516b50f
Afmer <grevich13@gmail.com>
2023-05-22 14:44:29 +0300

    Add role property

--- Models/UserDataModel.cs
+++ Models/UserDataModel.cs
@@ -1,14 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Features.Enums;
 namespace MVCSite.Models;
+[PrimaryKey(nameof(Login))]
 public class UserDataModel
 {
-    public int Id {get; set;}
-    public string Name {get; set;}
     public string Login {get; set;}
+    public string Name {get; set;}
     public string Password {get; set;}
-    public UserDataModel(string name, string login, string password)
+    public Role Role {get; set;}
+    public UserDataModel(string name, string login, string password, Role role)
     {
         Name = name;
         Login = login;
         Password = password;
+        Role = role;
     }
 }
