1e17aa9f7fd002f350c193c760e0045c55090048
Afmer <grevich13@gmail.com>
2023-05-22 15:38:15 +0300

    Add UserIdentityDataModel context

--- Features/MariaDB/MariaDbContext.cs
+++ Features/MariaDB/MariaDbContext.cs
@@ -1,8 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using MVCSite.Models;
+using Features.Extensions;
+using Features.Enums;
+using System.Text;
+
 namespace Features.MariaDB;
 
 public partial class MariaDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
+    public DbSet<UserIdentityDataModel> UserIdentity {get; set;} = null!;
     public MariaDbContext(DbContextOptions<MariaDbContext> options)
         : base(options)
     {
@@ -10,5 +16,8 @@ public partial class MariaDbContext : Microsoft.EntityFrameworkCore.DbContext
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<UserIdentityDataModel>().HasData(
+            new UserIdentityDataModel("admin", Encoding.ASCII.GetString(HashPassword.ComputePasswordHash("admin", 785433)), 785433, Role.Admin)
+        );
     }
 }
