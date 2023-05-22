34adce1b579a615f31912d38de3540bd37ba7b39
Afmer <grevich13@gmail.com>
2023-05-22 23:17:32 +0300

    Realize post request

--- Controllers/IdentityController.cs
+++ Controllers/IdentityController.cs
@@ -1,12 +1,12 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVCSite.Features.MariaDB;
+using MVCSite.Features.Extensions;
 namespace MVCSite.Controllers;
 public class IdentityController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<IdentityController> _logger;
     private readonly MariaDbContext _db;
-    public IdentityController(ILogger<HomeController> logger, MariaDbContext db)
+    public IdentityController(ILogger<IdentityController> logger, MariaDbContext db)
     {
         _logger = logger;
         _db = db;
@@ -15,5 +15,13 @@ public class IdentityController : Controller
     public IActionResult Login()
     {
         return View();
+    }
+    [HttpPost]
+    public IResult Login(string login, string password)
+    {
+        var user = _db.UserIdentity.Find(login);
+        if (user is null) return Results.Unauthorized();
+        if(!HashPassword.IsPasswordValid(password, user.Salt, user.Password)) return Results.Unauthorized();
+        return Results.Redirect("~/Home/Index");
     }
 }
