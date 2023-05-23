736076944fcb6f93bb5eaf5dea73ee0fe7a5dd03
Afmer <grevich13@gmail.com>
2023-05-23 11:56:45 +0300

    Add Identity token datamodel

---
+++ Models/IdentityTokenDataModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCSite.Models;
+[PrimaryKey(nameof(IdentityToken))]
+public class IdentityTokenDataModel
+{
+    [MaxLength(100)]
+    public string IdentityToken {get; set;}
+    public string Login {get; set;}
+    public IdentityTokenDataModel(string identityToken, string login)
+    {
+        IdentityToken = identityToken;
+        Login = login;
+    }
+}
