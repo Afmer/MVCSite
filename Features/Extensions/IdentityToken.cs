bb0b31f57e52c0ac7b5bdd3d63a0ad20b3c54899
Afmer <grevich13@gmail.com>
2023-05-24 15:40:57 +0300

    Add discription in IdentityToken

--- Features/Extensions/IdentityToken.cs
+++ Features/Extensions/IdentityToken.cs
@@ -3,10 +3,18 @@ using System.Text;
 namespace MVCSite.Features.Extensions;
 public static class IdentityToken
 {
+    ///<summary>
+    ///Генерирует строку длинной 100
+    ///(Generates a string of length 100)
+    ///</summary>
     public static string Generate()
     {
         return Generate(50);
     }
+    ///<summary>
+    ///Длина исходной строки будет равна length * 2
+    ///(The length of the original string will be equal to length * 2)
+    ///</summary>
     public static string Generate(int length)
     {
         byte[] byteString = new byte[length];
