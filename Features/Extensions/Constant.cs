099833a84edfc5b11b5226fd3fd25abbf8ab63e7
Afmer <grevich13@gmail.com>
2023-05-26 15:02:05 +0300

    Add constant TokenLifetimeRequirement

--- Features/Extensions/Constant.cs
+++ Features/Extensions/Constant.cs
@@ -3,4 +3,5 @@ public static class Constant
 {
     public const string IdentityToken = "identity_token";
     public const string AdminHierarchy = "admin_hierarchy";
+    public const string TokenLifetimeRequirement = "token_lifetime_requirement";
 }
