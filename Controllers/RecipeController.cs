c6e99f82b73dfadadfb3fdb400379f616a584baa
Afmer <afmerlord@gmail.com>
2023-07-06 19:16:05 +0300

    Initialize edit in Recipe controller

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MVCSite.Controllers;
 public class RecipeController : Controller
 {
     public IActionResult Index()
+    {
+        return View();
+    }
+    [Authorize]
+    public IActionResult Edit()
     {
         return View();
     }
