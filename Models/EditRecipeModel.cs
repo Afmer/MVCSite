0024b9c2b0d1d550cccfd486a390852b6feed15a
Afmer <afmerlord@gmail.com>
2023-07-27 13:49:39 +0300

    Initialize EditRecipeModel

---
+++ Models/EditRecipeModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCSite.Models;
+public class EditRecipeModel
+{
+    [Display(Name = "Оглавление")]
+    [Required]
+    public string? Label {get; set;}
+
+    [Display(Name = "Обложка")]
+    [Required]
+    public IFormFile? LabelImage {get; set;}
+
+    [Display(Name = "Содержимое")]
+    [Required]
+    public string? Content {get; set;}
+    [HiddenInput(DisplayValue = false)]
+    [Required]
+    public Guid RecipeId {get; set;}
+}
