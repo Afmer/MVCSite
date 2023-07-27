f209cfb5158a4825d1cca70e3334d26aec3c9346
Afmer <afmerlord@gmail.com>
2023-07-27 13:51:25 +0300

    Add edit form in Recipe/Edit

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -1,16 +1,45 @@
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MVCSite.Interfaces;
+using MVCSite.Models;
 
 namespace MVCSite.Controllers;
 public class RecipeController : Controller
 {
+    private IDBManager _dbManager;
+    public RecipeController(IDBManager dbManager)
+    {
+        _dbManager = dbManager;
+    }
     public IActionResult Index()
     {
         return View();
     }
     [Authorize]
-    public IActionResult Edit()
+    [HttpGet]
+    public IActionResult Edit(string id = "")
     {
-        return View();
+        if(id == "")
+        {
+            var model = new EditRecipeModel();
+            model.RecipeId = Guid.NewGuid();
+            return View(model);
+        }
+        else if(_dbManager.IsHasRecipe(new Guid(id)))
+        {
+            throw new NotImplementedException();
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+    }
+    [Authorize]
+    [HttpPost]
+    public IActionResult Edit(EditRecipeModel model)
+    {
+        Regex img = new(@"<img\s+[^>]*src=""(?<Link>[^""]*)""[^>]*>");
+        throw new NotImplementedException();
     }
 }
