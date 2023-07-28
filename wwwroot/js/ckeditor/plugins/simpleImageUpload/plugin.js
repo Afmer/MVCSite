4c4acddce339e870b100b2d6a3abfaa4a3357413
Afmer <afmerlord@gmail.com>
2023-07-28 20:11:07 +0300

    Add jpg coverter in upload image

--- wwwroot/js/ckeditor/plugins/simpleImageUpload/plugin.js
+++ wwwroot/js/ckeditor/plugins/simpleImageUpload/plugin.js
@@ -4,17 +4,27 @@ CKEDITOR.plugins.add( 'simpleImageUpload', {
         var fileDialog = $('<input type="file">');
         
         fileDialog.on('change', function (e) {
-            var uploadUrl = editor.config.uploadUrl;
 			var file = fileDialog[0].files[0];
-            if (file) {
-                const reader = new FileReader();
-                var base64Data;
-                reader.readAsDataURL(file);
-                reader.onload = function() {
-                    var ele = editor.document.createElement('img');
-                    ele.setAttribute('src', reader.result);
-                    editor.insertElement(ele);
+            if(file){
+                const jpgReader = new FileReader();
+                jpgReader.onload = function (e) {
+                    var img = new Image();
+                    
+                    img.onload = function () {
+                        var canvas = document.createElement('canvas');
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+                        var ctx = canvas.getContext('2d');
+                        ctx.drawImage(img, 0, 0);
+                        var jpgFile = canvas.toDataURL('image/jpeg', 1);
+                        var ele = editor.document.createElement('img');
+                        ele.setAttribute('src', jpgFile);
+                        editor.insertElement(ele);
+                    };
+                    
+                    img.src = e.target.result;
                 };
+                jpgReader.readAsDataURL(file);
             }
 
         })
