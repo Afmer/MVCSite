f82c2a19b6b97729a33ea2ce73fa13cb919baa79
Afmer <afmerlord@gmail.com>
2023-07-29 03:42:55 +0300

    Add cookie type recipe id

--- Features/Extensions/Constants/CookieType.cs
+++ Features/Extensions/Constants/CookieType.cs
@@ -2,4 +2,5 @@ namespace MVCSite.Features.Extensions.Constants;
 public static class CookieType
 {
     public const string IdentityToken = "identity_token";
+    public const string RecipeID = "recipe_id_cookie";
 }
