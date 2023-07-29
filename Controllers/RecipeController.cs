02252827046f5fc126159bd8d063fe6e4ac54d67
Afmer <afmerlord@gmail.com>
2023-07-29 17:14:38 +0300

    Add Recipe ediotr and shower

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -3,14 +3,18 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MVCSite.Interfaces;
 using MVCSite.Models;
+using MVCSite.Features.Extensions.Constants;
+using MVCSite.Features.Enums;
 
 namespace MVCSite.Controllers;
 public class RecipeController : Controller
 {
-    private IDBManager _dbManager;
-    public RecipeController(IDBManager dbManager)
+    private readonly IDBManager _dbManager;
+    private readonly IImageService _imageService;
+    public RecipeController(IDBManager dbManager, IImageService imageService)
     {
         _dbManager = dbManager;
+        _imageService = imageService;
     }
     public IActionResult Index()
     {
@@ -24,6 +28,14 @@ public class RecipeController : Controller
         {
             var model = new EditRecipeModel();
             model.RecipeId = Guid.NewGuid();
+            var cookieOptions = new CookieOptions
+            {
+                // Устанавливаем время жизни куки (опционально)
+                Expires = DateTime.Now.AddHours(24) // Куки будет действительно в течение 1 часа
+            };
+
+            // Устанавливаем значение куки
+            Response.Cookies.Append(CookieType.RecipeID, model.RecipeId.ToString(), cookieOptions);
             return View(model);
         }
         else if(_dbManager.IsHasRecipe(new Guid(id)))
@@ -37,9 +49,52 @@ public class RecipeController : Controller
     }
     [Authorize]
     [HttpPost]
-    public IActionResult Edit(EditRecipeModel model)
+    public async Task<IActionResult> Edit(EditRecipeModel model)
     {
-        Regex img = new(@"<img\s+[^>]*src=""(?<Link>[^""]*)""[^>]*>");
-        throw new NotImplementedException();
+        if(!ModelState.IsValid) return View(model);
+        var recipe = new RecipeDataModel();
+        recipe.Content = model.Content;
+        recipe.DateOfCreation = DateTime.UtcNow;
+        recipe.Id = model.RecipeId;
+        recipe.Label = model.Label;
+        var claim = HttpContext.User.FindFirst(c => c.Type == CookieType.IdentityToken);
+        IdentityTokenDataModel tokenData = null!;
+        if(claim != null)
+        {
+            var token = claim.Value;
+            tokenData = _dbManager.GetIdentityToken(token);
+            if(tokenData == null)
+                return Content("unknown error");
+        }
+        else
+        {
+            return Content("unknown error");
+        }
+        recipe.AuthorLogin = tokenData.Login;
+        var addRecipeResult = await _dbManager.AddRecipe(recipe);
+        var matches = Regex.Matches(model.Content!, @"<img\s+[^>]*src=""\/api\/Image\/Show\?id=(?<id>[^&]*)&amp;imageArea=RecipeImages""[^>]*>");
+        Response.Cookies.Delete(CookieType.RecipeID);
+        var ids = new Queue<Guid>();
+        foreach(Match match in matches)
+            ids.Enqueue(new Guid(match.Groups["id"].Value));
+        if(!addRecipeResult)
+            return Content("unknown error");
+        var CheckAndMigrateResult = await _dbManager.CheckAndMigrateTempImages(ids, model.RecipeId);
+        if(CheckAndMigrateResult.Status != MigrateTempImageStatusCode.Success)
+            return Content("unknown error");
+        foreach(var image in CheckAndMigrateResult.ImageForDelete)
+        {
+            await _imageService.Delete(image.Id, "RecipeImages");
+        }
+        return LocalRedirect("~/Home/Index");
+    }
+    [HttpGet]
+    public IActionResult Show(Guid id)
+    {
+        var recipe = _dbManager.GetRecipe(id);
+        if(recipe != null && recipe.Content != null)
+            return View(new ShowModel(){Content = recipe.Content});
+        else
+            return Content("");
     }
 }
